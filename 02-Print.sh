9301c774011ec71e2f04f3fce558c65ef97d7fc3
EC2 Default User <ec2-user@ip-172-31-55-85.ec2.internal>
2021-09-07 13:10:11 +0000

    Color codes

---
+++ 02-Print.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "\e[1;31mText in Red"
+echo -e "\e[1;32mText in Green"
+echo -e "\e[1;33mText in Yellow"
+echo -e "\e[1;34mText in Blue"
+echo -e "\e[1;35mText in Magenta"
+echo -e "\e[1;36mText in Cyan"
+
+
+echo -e "\e[31mText in color; but disabled now on\e[0m"
+echo -e "Text in no colour"
