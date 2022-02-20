407dc8e647dfce8675e5619b3710fa9a5da49130
Katy <katyaynilearning@gmail.com>
2022-02-20 10:31:30 +0530

    updating read commands

--- 04-inputRead.sh
+++ 04-inputRead.sh
@@ -1,2 +1,6 @@
-read -p "Enter you name:" Name
-echo "Welcome" $Name
+read -p "enter you name:" name
+echo "Welcome $name"
+read -p "Course name please" course
+echo "Please login to your batch $course"
+
+
