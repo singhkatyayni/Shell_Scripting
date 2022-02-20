65cd72b5274ac742a7ca6817b8f75c4fd3c0397b
Katy <katyaynilearning@gmail.com>
2022-02-20 11:53:20 +0530

    File comparison

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-if [ -f components/$1.sh]
-bash components/$1.sh
+if [ -f components/$1.sh ]; then
+ bash components/$1.sh
 else
   echo "Wrong input"
   echo "Available inputs : frontend|shipping|payment|cart|catalogue|rabbitmq|redis|user|dispatch|mongodb|mysql"
