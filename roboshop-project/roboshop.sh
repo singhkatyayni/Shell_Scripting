b06aad7c10915c123c479e06c18e40e38fe49ed3
Katy <katyaynilearning@gmail.com>
2022-02-20 11:50:30 +0530

    if conditions

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
+if [ -f components/$1.sh]
 bash components/$1.sh
+else
+  echo "Wrong input"
+  echo "Available inputs : frontend|shipping|payment|cart|catalogue|rabbitmq|redis|user|dispatch|mongodb|mysql"
+fi
