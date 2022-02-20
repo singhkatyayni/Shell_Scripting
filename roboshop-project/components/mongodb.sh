a437849daa8a7052f75c8272031ab6e97171fa0a
Katy <katyaynilearning@gmail.com>
2022-02-20 18:04:04 +0530

    Updating Shell_Scripting

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -56,8 +56,8 @@ echo "Update mongodb config file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
 
 echo "Start Database"
-systemctl enable mongodb &>>$LOG_FILE
-systemctl start mongodb &>>$LOG_FILE
+systemctl enable mongod &>>$LOG_FILE
+systemctl start mongod &>>$LOG_FILE
 
 echo "Download Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
