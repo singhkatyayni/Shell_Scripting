9d364b4b159754e89788c8b1b9a36185e127c30a
Katy <katyaynilearning@gmail.com>
2022-02-21 09:27:46 +0530

    Updating Shell_Scripting

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -10,8 +10,9 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 
 if [ -z "${AMI_ID}" ]; then
  echo -e "\e[1;31mUnable to find image AMIID\e[0m"
+ exit
 else
  echo -e "\e[1;32mAMI ID = ${AMI_ID}\e[0m"
 fi
 
-aws ec2 run-instances --image-id ami-${AMI_ID} --instance-type t2.micro --output text
+aws ec2 run-instances --image-id ami-${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}]"
