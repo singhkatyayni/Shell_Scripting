11d91357eba8f7cc2ec1fef546984940ca7e6d93
Katy <katyaynilearning@gmail.com>
2022-02-21 10:53:58 +0530

    Updating Shell_Scripting

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -17,6 +17,13 @@ fi
 
 PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANCE_NAME} --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 if [ -z "{PRIVATE_IP" ]; then
+# Find Security Group
+    SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=allow-all-ports --query "SecurityGroups[*].GroupId" --output text)
+    if [ -z "${SG_ID}" ]; then
+      echo -e "\e[1;33m Security Group allow-all-ports does not exist"
+      exit
+    fi
+# Creating Instance
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
 else
   echo -e "\e[1;33mInstance ${INSTANCE_NAME} already exists\e[0m"
