4a78dd616614c647bca27e92ff2868f449042ed4
Katy <katyaynilearning@gmail.com>
2022-02-21 09:03:15 +0530

    Updating Shell_Scripting

---
+++ roboshop-project/create-ec2.sh
@@ -0,0 +1,5 @@
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+
+if ( -z "${AMI_ID"); then
+ echo "Unable to find image AMIID"
+fi
