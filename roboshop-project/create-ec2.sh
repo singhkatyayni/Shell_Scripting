a9ead71b35a649b9fa3df5c2722329e270464a63
Katy <katyaynilearning@gmail.com>
2022-02-21 09:22:00 +0530

    Updating Shell_Scripting

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+INSTANCE_NAME=$1
+if [ -z "${INSTANCE_NAME}" ]; then
+  echo -e "\e[1;33mInstance name argument is needed\e[0m"
+fi
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 if [ -z "${AMI_ID}" ]; then
