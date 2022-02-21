56055c16e435d2e4cafc44136c0be51b04bab6e7
Katy <katyaynilearning@gmail.com>
2022-02-21 09:29:43 +0530

    Updating Shell_Scripting

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -15,4 +15,4 @@ else
  echo -e "\e[1;32mAMI ID = ${AMI_ID}\e[0m"
 fi
 
-aws ec2 run-instances --image-id ami-${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}]"
+aws ec2 run-instances --image-id ami-${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[Key=Name,Value=${INSTANCE_NAME}]"
