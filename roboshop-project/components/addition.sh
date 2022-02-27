a0b3b27d2ce8dbfc132ba06bad8e5f5d74406b2c
Katy <katyaynilearning@gmail.com>
2022-02-27 14:28:08 +0530

    Addition of two argument numbers

--- roboshop-project/components/addition.sh
+++ roboshop-project/components/addition.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-n1= "$1"
-n2= "$2"
-if [ n1 -gt 0 && n2 -gt 0 ];
-then Add= "$((n1 + n2))"
+n1="$1"
+n2="$2"
+if [ $n1 -gt 0 && $n2 -gt 0 ];
+then Add="$((n1 + n2))"
 echo "Adding the two numbers $Add"
 else
   echo "Arguments not given"
